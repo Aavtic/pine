ff514e7b5dbf00dd1e09c128c38773efe99d95ec
Aadish <aadishmkerala@gmail.com>
2026-02-08 23:31:03 +0530

    feat: runtime c abi for print and println

---
+++ runtime/c/pine_print.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int pine_print(const char* s) {
+    return printf("%s", s);
+}
---
+++ runtime/c/pine_println.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int pine_println(const char* s) {
+    return printf("%s\n", s);
+}
