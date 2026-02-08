4fd38e2a01349b539d27a9071f9b85e3b80ee541
Aadish <aadishmkerala@gmail.com>
2026-02-08 12:05:24 +0530

    feat: implemented pine runtime!, added string functionality by c ABI

--- crates/linker/src/linker.rs
+++ crates/linker/src/linker.rs
@@ -1,4 +1,5 @@
 use std::{io::Error, process::Output};
+use utils::get_all_files_in_dir;
 
 use inkwell::{
     module::Module,
@@ -34,21 +35,58 @@ impl ObjectCompiler {
 }
 
 impl ObjectLinker {
-    pub fn link(module_name: &str, output: &str) -> Result<Output, Error> {
+    pub fn link(module_name: &str, output: &str, runtime_objects: Option<Vec<String>>) -> Result<Output, Error> {
         let mut output_path = output.to_string();
         if cfg!(windows) && !output.contains(".exe") {
             output_path = format!("{}.exe", output_path);
         }
 
-        let input = format!("{}.o", module_name);
-        let linker_output = std::process::Command::new("cc")
-            .arg(input.clone())
-            .arg("-o")
-            .arg(output_path)
-            .output();
+        if let Some(runtime_objs) = runtime_objects {
+            let input = format!("{}.o", module_name);
+            let linker_output = std::process::Command::new("cc")
+                .arg(input.clone())
+                .args(runtime_objs)
+                .arg("-o")
+                .arg(output_path)
+                .output();
 
-        std::fs::remove_file(input).expect("Unable to delete object file");
-        linker_output
+            std::fs::remove_file(input).expect("Unable to delete object file");
+            linker_output
+        } else {
+            let input = format!("{}.o", module_name);
+            let linker_output = std::process::Command::new("cc")
+                .arg(input.clone())
+                .arg("-o")
+                .arg(output_path)
+                .output();
+
+            std::fs::remove_file(input).expect("Unable to delete object file");
+            linker_output
+        }
+    }
+
+    // output is only used to identify if it is windows or linux
+    pub fn compile_runtime(module_name: &str) -> Option<Vec<String>> {
+        let runtime_path = "./runtime/c/";
+        let runtime_files = get_all_files_in_dir(runtime_path);
+
+        if runtime_files.len() == 0 { return None }
+
+        let mut object_files = Vec::new();
+
+        for runtime_file in runtime_files {
+            let out_file = std::path::Path::new(&runtime_file).file_name().unwrap().to_str().unwrap();
+            let output = format!("{}_{}", module_name, out_file.replace(".c", ".o"));
+            std::process::Command::new("cc")
+                .arg("-c")
+                .arg(runtime_file)
+                .arg("-o")
+                .arg(output.clone())
+                .output().unwrap();
+            object_files.push(output);
+        }
+
+        return Some(object_files);
     }
 }
 
--- tools/pinec/src/utils.rs
+++ tools/pinec/src/utils.rs
@@ -1,2 +1,6 @@
 fn read_from_file(file_name: &str) {
 }
+
+
+fn get_all_files_in_dir(dir_path: &str) -> Vec<String> {
+}
