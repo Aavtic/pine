3b6cd7596a86614eb6a23745fa0cddfc66760f1f
Aadish <aadishmkerala@gmail.com>
2026-02-23 14:10:23 +0530

    Improved verbosity

--- tools/pinec/src/main.rs
+++ tools/pinec/src/main.rs
@@ -11,6 +11,8 @@ use linker::linker;
 
 use utils::handle_reading_file;
 
+const PROJECT_ROOT: &str = "main";
+
 /// Pine Compiler
 #[derive(ClapParser, Debug)]
 #[clap(version)]
@@ -89,7 +91,7 @@ fn _print_ast_to_dot(source_path: PathBuf, out_file: PathBuf) {
     let parent = PathBuf::from(source_path.parent().expect("Unable to find parent for source file"));
     let mut parser = Parser::new(tokens, parent);
 
-    parser.parse("main").unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
+    //parser.parse("main").unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
     //let mut ast = parser.get_compilation_unit().get_module("main").unwrap().ast.clone();
 
     unimplemented!();
@@ -110,7 +112,7 @@ fn build_ast(source_path: PathBuf) {
     let parent = PathBuf::from(source_path.parent().expect("Unable to find parent for source file"));
     let mut parser = Parser::new(tokens, parent);
 
-    parser.parse("main").unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
+    parser.parse(PROJECT_ROOT, vec![PROJECT_ROOT.into()], true).unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
     let ast = parser.get_compilation_unit();
 
     println!("{:#?}", ast);
@@ -119,19 +121,23 @@ fn build_ast(source_path: PathBuf) {
 fn build_analyze(file: PathBuf) {
     let source = handle_reading_file(&file);
     let tokens = lex(source.as_str());
-    let parent = PathBuf::from(file.parent().expect("Unable to find parent for source file"));
+    let parent = PathBuf::from(
+        file.parent()
+            .expect("Unable to find parent for source file"),
+    );
     let mut parser = Parser::new(tokens, parent);
-    parser.parse("main").unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
+    let namespace = vec![PROJECT_ROOT.into()];
+    parser
+        .parse(PROJECT_ROOT, namespace.clone(), true)
+        .unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
 
     //let mut ast = parser.get_compilation_unit().get_module("main").unwrap().ast.clone();
     let mut compilation_unit = parser.get_compilation_unit().clone();
 
-    let file_name = file.to_str().unwrap();
-
     let mut analyzer = Analyzer::new();
     if let Err(err) = analyzer.start_analysis(&mut compilation_unit) {
         eprintln!("Type Check failed due to:\n{}", err);
-        return
+        return;
     }
 
     println!("{:#?}", compilation_unit);
@@ -140,31 +146,32 @@ fn build_analyze(file: PathBuf) {
 fn build_llvm_ir(file: PathBuf) {
     let source = handle_reading_file(&file);
     let tokens = lex(source.as_str());
-    let parent = PathBuf::from(file.parent().expect("Unable to find parent for source file"));
+    let parent = PathBuf::from(
+        file.parent()
+            .expect("Unable to find parent for source file"),
+    );
     let mut parser = Parser::new(tokens, parent);
-    parser.parse("main").unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
+    let namespace = vec!["main".into()];
+    parser
+        .parse(PROJECT_ROOT, namespace.clone(), true)
+        .unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
 
     //let mut ast = parser.get_compilation_unit().get_module("main").unwrap().ast.clone();
     let mut compilation_unit = parser.get_compilation_unit().clone();
 
-    let file_name = file.to_str().unwrap();
-
     let mut analyzer = Analyzer::new();
     if let Err(err) = analyzer.start_analysis(&mut compilation_unit) {
         eprintln!("Type Check failed due to:\n{}", err);
-        return
+        return;
     }
 
+    let file_name = file.to_str().unwrap();
     let module_name = file_name
         .split(".")
         .next()
         .map(|n| n.to_string())
         .unwrap_or(file_name.replace(".alp", ""));
 
-
-    let ctx = CodeGen::create_context();
-    let mut codegen = CodeGen::new(&ctx, &module_name);
-    //println!("{:#?}", &ast);
 
     let ctx = CodeGen::create_context();
     let mut codegen_mod = CodeGenModules::new();
@@ -183,27 +190,31 @@ fn build_llvm_ir(file: PathBuf) {
         }
         println!(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
     }
-
 }
 
 fn build_object(file: PathBuf) {
     let source = handle_reading_file(&file);
     let tokens = lex(source.as_str());
-    let parent = PathBuf::from(file.parent().expect("Unable to find parent for source file"));
+    let parent = PathBuf::from(
+        file.parent()
+            .expect("Unable to find parent for source file"),
+    );
     let mut parser = Parser::new(tokens, parent);
-    parser.parse("main").unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
+    let namespace = vec!["main".into()];
+    parser
+        .parse("main", namespace.clone(), true)
+        .unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
 
     //let mut ast = parser.get_compilation_unit().get_module("main").unwrap().ast.clone();
     let mut compilation_unit = parser.get_compilation_unit().clone();
 
-    let file_name = file.to_str().unwrap();
-
     let mut analyzer = Analyzer::new();
     if let Err(err) = analyzer.start_analysis(&mut compilation_unit) {
         eprintln!("Type Check failed due to:\n{}", err);
-        return
+        return;
     }
 
+    let file_name = file.to_str().unwrap();
     let module_name = file_name
         .split(".")
         .next()
@@ -246,21 +257,26 @@ fn print_tokens(file: PathBuf) {
 fn compile_program(file: PathBuf) {
     let source = handle_reading_file(&file);
     let tokens = lex(source.as_str());
-    let parent = PathBuf::from(file.parent().expect("Unable to find parent for source file"));
+    let parent = PathBuf::from(
+        file.parent()
+            .expect("Unable to find parent for source file"),
+    );
     let mut parser = Parser::new(tokens, parent);
-    parser.parse("main").unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
+    let namespace = vec!["main".into()];
+    parser
+        .parse(PROJECT_ROOT, namespace.clone(), true)
+        .unwrap_or_else(|err| panic!("Couldn't parse the program due to: \n{}", err));
 
     //let mut ast = parser.get_compilation_unit().get_module("main").unwrap().ast.clone();
     let mut compilation_unit = parser.get_compilation_unit().clone();
 
-    let file_name = file.to_str().unwrap();
-
     let mut analyzer = Analyzer::new();
     if let Err(err) = analyzer.start_analysis(&mut compilation_unit) {
         eprintln!("Type Check failed due to:\n{}", err);
-        return
+        return;
     }
 
+    let file_name = file.to_str().unwrap();
     let module_name = file_name
         .split(".")
         .next()
@@ -276,13 +292,13 @@ fn compile_program(file: PathBuf) {
             panic!("{:?}", err);
     });
 
-
     let mut object_files = Vec::new();
     for (module_name, module_ref) in modules {
         println!("Compiling {}", module_name);
         let obj_file = linker::ObjectCompiler::compile_module(&module_ref, &module_name, file.parent().unwrap());
         object_files.push(obj_file);
     }
+    println!("Building {}", module_name);
     linker::ObjectLinker::link(&module_name, object_files, linker::ObjectLinker::compile_runtime(&module_name)).unwrap();
 }
 
